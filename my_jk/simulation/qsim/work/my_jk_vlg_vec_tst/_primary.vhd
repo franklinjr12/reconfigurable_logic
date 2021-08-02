a70bacc8e7acd9eb669f5de90d217c22fbcc461c
frankjr12 <franklinjunior12@hotmail.com>
2021-08-02 17:37:38 -0300

    added jk ff

---
+++ my_jk/simulation/qsim/work/my_jk_vlg_vec_tst/_primary.vhd
@@ -0,0 +1,4 @@
+library verilog;
+use verilog.vl_types.all;
+entity my_jk_vlg_vec_tst is
+end my_jk_vlg_vec_tst;
---
+++ vhdl_test/simulation/qsim/work/vhdl_test_vlg_vec_tst/_primary.vhd
@@ -0,0 +1,4 @@
+library verilog;
+use verilog.vl_types.all;
+entity vhdl_test_vlg_vec_tst is
+end vhdl_test_vlg_vec_tst;
